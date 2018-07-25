866946202651f1b5566d2ac082637d58baa34fa1
gabrielcoder <gabrielcoder247@gmail.com>
2018-07-25 12:18:30 +0300

    add bootstrap link to the global css

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -7,11 +7,11 @@ export class DateCountPipe implements PipeTransform {
 
   transform(value: any): number {
     let today:Date = new Date(); // get current date and time
-    let todayWithoutime:any = new Date(today.getFullYear(), today.getMonth(),today.getDate())
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(),today.getDate())
     var dateDifference = Math.abs(value-todayWithNoTime) //return value in milliseconds
     const secondsInADay = 86400; // 60 seconds*minutes in an hour * 24 hours
     var dateDifferenceSeconds=dateDifference*0.001;//convert to seconds
-    var dateCounter = dateDiffereceSeconds/secondsInADay;
+    var dateCounter = dateDifferenceSeconds/secondsInADay;
     if(dateCounter >= 1){
       return dateCounter;
     }else{
