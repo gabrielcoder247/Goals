ec95e1e9975a4f42e71c2e046463578803a1e99c
gabrielcoder <gabrielcoder247@gmail.com>
2018-07-30 11:44:26 +0300

    create a getGoal method for goal-service.ts

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -5,6 +5,7 @@ import {Goal} from '../goal';
 import {Quote} from '../quote-class/quote'
 import {HttpClient} from '@angular/common/http'
 import {QuoteRequestService} from '../quote-http/quote-request.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
@@ -12,13 +13,16 @@ import {QuoteRequestService} from '../quote-http/quote-request.service';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+  goToUrl(id){
+    this.router.navigate(['/goals',id])
+  }
 
 
-  goals:Goal[];
+  goals:Goal[]; 
   alertService:AlertService;
   quote:Quote;
 
-  constructor(goalService:GoalService,alertService:AlertService,private http:HttpClient, private quoteService:QuoteRequestService) {
+  constructor(goalService:GoalService,alertService:AlertService,private http:HttpClient, private quoteService:QuoteRequestService,private router:Router) {
     this.goals = goalService.getGoals();
     this.alertService=alertService;//make the service available to the class
    }
@@ -33,14 +37,13 @@ export class GoalComponent implements OnInit {
        goal.completeDate = new Date(goal.complete)
        this.goals.push(goal)
      }
-     deleteGoal(isComplete,index){
-       if(isComplete){
+     deleteGoal(index){
          let toDelete=confirm('Are you sure you want to delete ${this.goals[index].name}')
+
          if (toDelete){
            this.goals.splice(index,1)
            this.alertService.alertMe('Goal has been deleted')
          }
-       }
      }
   
   ngOnInit() {
--- src/app/goals/goal.service.ts
+++ src/app/goals/goal.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import {Goals} from '../goals';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class GoalService {
   getGoals(){
     return Goals;
   }
-
+getGoal(id){
+  for (let goal of Goals){
+    if(goal.id ==id){
+      return goal;
+    }
+  }
+}
   constructor() { }
 }
